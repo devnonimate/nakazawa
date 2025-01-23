a677d6cbaacd5825067c07737adc307f255c8213
devnonimate <devprojectnewera@gmail.com>
2025-01-23 09:42:55 -0300

    Mais duas

--- src/pages/config.js
+++ src/pages/config.js
@@ -1,5 +1,5 @@
 const config = {
-    API_URL: 'https://d9d5-2804-71d4-6005-53f0-ac89-b5fa-1ab4-650f.ngrok-free.app',
+    API_URL: 'https://7522-2804-71d4-6005-53f0-ac89-b5fa-1ab4-650f.ngrok-free.app/',
   };
   
   export default config;
